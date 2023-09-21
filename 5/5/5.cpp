fb02cd3af4cc77e1f695205baae0ff317105b2bf
Vandeuu <145645102+Vandeuu@users.noreply.github.com>
2023-09-21 12:50:48 +0600

    Add files via upload

---
+++ 5/5/5.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int* clear_function(int* array, int arrSize)
+{
+    int index;
+    std::cin >> index;
+    for (int i = index; i < arrSize; i++)
+    {
+        array[i] = NULL;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int arrSize;
+    std::cin >> arrSize;
+    int* arr = new int[arrSize];
+    for (int i = 0; i < arrSize; i++)
+    {
+        arr[i] = i + 1;
+    }
+
+
+
+    clear_function(arr, arrSize);
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << *&arr[i];
+    }
+}
