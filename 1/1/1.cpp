a117e787bb2463bf3a5e99b794320fa25221f8fc
Vandeuu <145645102+Vandeuu@users.noreply.github.com>
2023-09-21 12:49:16 +0600

    Add files via upload

---
+++ 1/1/1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int insert_arr(int insertIndex)
+{
+    cin >> insertIndex;
+    int result;
+    result = insertIndex;
+    return result;
+}
+
+int main()
+{
+    int arr[10];
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i] = NULL;
+    }
+
+    int index = 0;
+
+    for (int i = insert_arr(index); i < 10; i++)
+    {
+        arr[i] = i;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
